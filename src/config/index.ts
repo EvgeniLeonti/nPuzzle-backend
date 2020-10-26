5a1c0fb9861666fc3cfb47ec2b8b752449a8dcbb
evgeni <evgeni@ravin-ai.com>
2020-10-26 19:43:11 +0200

    API ENDPOINT update

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,11 @@
 //TODO use dotenv
 
 export default {
-  port: parseInt(process.env.PORT, 10) || 80,
+  port: parseInt(process.env.PORT, 10) || 8080,
   logs: {
     level: process.env.LOG_LEVEL || 'info',
   },
   api: {
-    prefix: '/api', // todo version
+    prefix: '/api/v1'
   }
 };
