fe8662036e73dbff82972c1296e08d39131bec28
evgeni <evgeni@ravin-ai.com>
2020-10-27 11:47:57 +0200

    dotenv as dev dependency

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,9 @@
-import dotenv from 'dotenv';
-
-const envFound = dotenv.config();
-if (envFound.error) {
-  throw new Error(`Couldn't find .env file`);
+if (process.env.ENV !== 'production') {
+  const dotenv = require('dotenv');
+  const envFound = dotenv.config();
+  if (envFound.error) {
+    throw new Error(`Couldn't find .env file`);
+  }
 }
 
 export default {
